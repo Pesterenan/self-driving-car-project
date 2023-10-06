7fb15ec3e0ac9089088e2272a715f4475c77d303
Pesterenan <pesterenan@gmail.com>
2023-10-06 11:36:44 -0300

    [SFCP]: First commit with code from 2 videos

---
+++ world/js/math/graph.js
@@ -0,0 +1,75 @@
+// Graphs are data structures made of nodes and links ( vertices and edges )
+// G = (V, E)
+
+class Graph {
+  constructor(points = [], segments = []) {
+    this.points = points;
+    this.segments = segments;
+  }
+
+  addPoint(point) {
+    this.points.push(point);
+  }
+
+  addSegment(segment) {
+    this.segments.push(segment);
+  }
+
+  containsPoint(point) {
+    return this.points.find((p) => p.equals(point));
+  }
+
+  containsSegment(segment) {
+    return this.segments.find((p) => p.equals(segment));
+  }
+  tryAddPoint(point) {
+    if (!this.containsPoint(point)) {
+      this.addPoint(point);
+      return true;
+    }
+    return false;
+  }
+
+  tryAddSegment(segment) {
+    if (!this.containsSegment(segment) && !segment.p1.equals(segment.p2)) {
+      this.addSegment(segment);
+      return true;
+    }
+    return false;
+  }
+  removeSegment(segment) {
+    this.segments.splice(this.segments.indexOf(segment), 1);
+  }
+  removePoint(point) {
+    const segments = this.getSegmentsWithPoint(point);
+    for (const seg of segments) {
+      this.removeSegment(seg);
+    }
+    this.points.splice(this.points.indexOf(point), 1);
+  }
+
+  getSegmentsWithPoint(point) {
+    const segments = [];
+    for (const seg of this.segments) {
+      if (seg.includes(point)) {
+        segments.push(seg);
+      }
+    }
+    return segments;
+  }
+
+  dispose() {
+    this.points.length = 0;
+    this.segments.length = 0;
+  }
+
+  draw(ctx) {
+    for (const seg of this.segments) {
+      seg.draw(ctx);
+    }
+
+    for (const point of this.points) {
+      point.draw(ctx);
+    }
+  }
+}
